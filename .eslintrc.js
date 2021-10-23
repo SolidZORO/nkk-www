227e0f044eefe36e9db324be8b457672da0bba6a
Jason Feng <solidzoro@live.com>
2021-10-23 14:16:03 +0800

    fix: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@
 module.exports = {
   // https://github.com/qcolate/web-configs/blob/master/packages/eslint-config/react.js
   // extends: ['airbnb', 'airbnb-typescript', 'prettier'],
-  extends: ['next/core-web-vitals', '@qcolate/eslint-config'],
+  extends: ['@qcolate/eslint-config'],
+  root: true,
   env: {
     browser: true,
     es2021: true,
@@ -18,16 +19,9 @@ module.exports = {
     'max-len': [
       2,
       {
-        code: 80, // (default 80) enforces a maximum line length
-        ignoreComments: true, // ignores all trailing comments and comments on their own line
-        ignoreTrailingComments: true, // ignores only trailing comments
-        // ignoreUrls: true, // ignores lines that contain a URL
-        // tabWidth: 4, // (default 4) specifies the character width for tab characters
-        // comments: 80, // enforces a maximum line length for comments; defaults to value of code
-        // ignorePattern: true, // ignores lines matching a regular expression; can only match a single line and need to be double escaped when written in YAML or JSON
-        // ignoreStrings: true, // ignores lines that contain a double-quoted or single-quoted string
-        // ignoreTemplateLiterals: true, // ignores lines that contain a template literal
-        // ignoreRegExpLiterals: true, // ignores lines that contain a RegExp literal
+        code: 80, // (default 80)
+        ignoreComments: true,
+        ignoreTrailingComments: true,
       },
     ],
     'spaced-comment': 0,
