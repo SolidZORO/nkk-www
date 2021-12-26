77dd8b53de97ceb4c4e18ac294d77713a9893aaf
Jason Feng <solidzoro@live.com>
2021-12-26 17:00:25 +0800

    feat: 优化 默认 logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,8 @@ import { _getServerSideGlobalProps } from '@/_getServerSideGlobalProps';
 
 export { Home as default } from '@/page-components/index/Home/Home';
 
-// 简写
+// 简写（让 server 去拿基础的 props，比如全局 setting）
 export const getServerSideProps = _getServerSideGlobalProps;
-// export const getServerSideProps = _getServerSideProps;
 
 // 适合扩展（props 还需要追加 data）
 // export const getServerSideProps: GetServerSideProps = async (ctx) => {
